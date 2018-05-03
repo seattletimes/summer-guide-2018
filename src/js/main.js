c964caff617f97e98b6a9361fec26b9a8f7c6aef
Thomas Wilburn <tbone@thomaswilburn.net>
2018-05-03 16:22:55 -0700

    Comments

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ var dot = require("./lib/dot");
 var listTemplate = dot.compile(require("./_list.html"));
 var listContainer = $.one(".event-listings");
 
+// Pre-process data, adding a months array based on the dates specified
 window.eventData.forEach(function(row) {
   if (row.date) {
     row.months = [ row.date.split("/")[0] ];
@@ -14,16 +15,17 @@ window.eventData.forEach(function(row) {
     var startMonth = 5;
     var endMonth = 9;
     if (row.start) {
-      [startMonth] = row.start.split("/");
+      [startMonth] = row.start.split("/") * 1;
     }
     if (row.end) {
-      [endMonth] = row.end.split("/");
+      [endMonth] = row.end.split("/") * 1;
     }
     row.months = [];
     for (var i = startMonth; i <= endMonth; i++) row.months.push(i);
   }
 });
 
+// Test: do two lists have items in common?
 var intersects = function(a, b) {
   var aMap = {};
   a.forEach(k => aMap[k] = true);
@@ -33,26 +35,33 @@ var intersects = function(a, b) {
   return false;
 };
 
+// Show items matching the months array
 var filterMonths = function(list, months) {
   if (!months || !months.length) return list;
   return list.filter(d => intersects(d.months, months));
 };
 
+// Show items included in a list of categories
 var filterCategories = function(list, cats) {
   if (!cats || !cats.length) return list;
   return list.filter(d => !d.category || cats.indexOf(d.category) > -1);
 }
 
+// Apply all filters to the full list
 var applyFilters = function() {
+  // Get form inputs
   var categories = $(".category:checked").map(c => c.value);
   var months = $(".month:checked").map(m => m.value).sort();
+  // Set up a filter chain, in which the result of each step is passed into the next
   var filters = [
     list => filterMonths(list, months),
     list => filterCategories(list, categories)
   ];
+  // Start with the full list of events
   var list = window.eventData;
+  // Run through the filter chain, progressively updating `list`
   filters.forEach(f => list = f(list));
-  console.log(list);
+  // Output HTML into template from final results
   listContainer.innerHTML = listTemplate(list);
 };
 
