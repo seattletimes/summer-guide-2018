b669465627bfa23c88a66868d18a412f529ad811
Alec Glassford <glassford@cs.stanford.edu>
2018-05-09 15:23:11 -0700

    Make search work

--- src/js/list.js
+++ src/js/list.js
@@ -6,22 +6,35 @@ var { intersects, filterCategories } = require("./util");
 var listTemplate = dot.compile(require("./_list.html"));
 var listContainer = $.one(".event-listings");
 
+var searchBox = $.one(".search input");
+
 // Show items matching the months array
 var filterMonths = function(list, { months }) {
   if (!months || !months.length) return list;
   return list.filter(d => intersects(d.months, months));
 };
 
+// Filter only items that match text in the search box
+var filterBySearch = function(list, { query }) {
+  if (!query) return list;
+  var re = new RegExp(query, "i");
+  var attrs = [ 'name', 'location', 'url', 'details', 'description'];
+  return list.filter(r => attrs.some(attr => r[attr] ? r[attr].match(re) : false));
+};
+
 // Apply all filters to the full list
 var applyFilters = function() {
+  console.log('apply');
   // Get form inputs
   var categories = $(".category:checked").map(c => c.value);
   var months = $(".month:checked").map(m => m.value).sort();
-  var config = { categories, months };
+  var query = searchBox.value;
+  var config = { categories, months, query };
   // Set up a filter chain, in which the result of each step is passed into the next
   var filters = [
     filterMonths,
-    filterCategories
+    filterCategories,
+    filterBySearch
   ];
   // Start with the full list of events
   var list = window.eventData;
@@ -33,6 +46,8 @@ var applyFilters = function() {
 
 var filterContainer = $.one(".filters");
 filterContainer.addEventListener("change", applyFilters);
+
+searchBox.addEventListener("keyup", applyFilters);
 
 module.exports = function() {
   applyFilters();
